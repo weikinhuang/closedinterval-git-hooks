a858429707253bd67fba86c4a427627d33bfbd1a
Wei Kin Huang <weikin.huang04@gmail.com>
2013-07-10 13:08:50 -0400

    Rename npm.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 DIRECTORY="$(git rev-parse --show-toplevel)"
+cd "${DIRECTORY}"
+
+composer install
+composer update
+
 cd "${DIRECTORY}/.build"
 
 npm install --no-bin-links
+npm update --no-bin-link
