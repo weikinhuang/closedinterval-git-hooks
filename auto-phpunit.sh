462f4c6caa3b3066b2301948adbb3f21f1544607
Wei Kin Huang <weikin.huang04@gmail.com>
2013-07-16 15:58:39 -0400

    Adding mocks for solr and gearman

---
+++ auto-phpunit.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# auto rsync daemon similar to lsyncd
+DIRECTORY="$(git rev-parse --show-toplevel)"
+CHANGE_DELAY=5
+DIRECTORY_SEPARATOR='/'
+
+# if we're in cygwin, clean up for windows inotifywait
+if type cygpath &> /dev/null; then
+	DIRECTORY="$(cygpath -w "$DIRECTORY")"
+	DIRECTORY_SEPARATOR='\\'
+fi
+
+DIRMD5=$(echo "$DIRECTORY" | md5sum -t | cut -f1 -d' ')
+LOCKFILE="/tmp/phpunitdev-$DIRMD5"
+cd "$DIRECTORY"
+
+function phpunit-files() {
+	if [[ -e "$LOCKFILE" ]]; then
+		return 1
+	fi
+	touch "$LOCKFILE"
+	sleep $CHANGE_DELAY
+	bash vendor/bin/phpunit
+	rm -f "$LOCKFILE"
+}
+
+# cleanup from previous process
+rm -f "$LOCKFILE"
+
+if type inotifywait &> /dev/null; then
+
+inotifywait -mrq --format '%e %w %f' \
+"${DIRECTORY}${DIRECTORY_SEPARATOR}classes" \
+| \
+while read event path file; do
+	sleep 0.2
+	phpunit-files &
+done
+
+fi
