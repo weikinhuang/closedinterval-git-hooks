ee98ed8ef2f768df2f5a77db909710fa729a3b57
Wei Kin Huang <weikin.huang04@gmail.com>
2015-04-01 11:27:59 -0400

    Allow running hooks from .git-hooks directory

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 "use strict";
 
-var runner;
+const Bluebird = require("bluebird"),
+	fs = require("fs"),
+	fileHookRunner = require("./lib/file-hook-runner"),
+	path = require("path");
+
+var cliArgs = process.argv.slice(3),
+	promises = [];
+
+// check the built in runner
 try {
-	runner = require("./lib/hooks/" + process.argv[2]);
-	runner(process.argv.slice(3))
-		.then(function() {
-			setTimeout(function() {
-				process.exit(0);
-			}, 15);
-		})
-		.catch(function() {
-			setTimeout(function() {
-				process.exit(1);
-			}, 15);
+	promises.push(require("./lib/hooks/" + process.argv[2]));
+} catch (e) {
+	// empty
+}
+// next try to load any built into the repo
+try {
+	fs.readdirSync(path.join(process.cwd(), ".git-hooks", process.argv[2]))
+		.forEach(function(file) {
+			promises.push(
+				fileHookRunner(
+					path.join(process.cwd(), ".git-hooks", process.argv[2], file)
+				)
+			);
 		});
 } catch (e) {
 	// empty
 }
+
+Bluebird.reduce(promises, function(memo, fn) {
+	return fn(cliArgs);
+}, 0)
+	.then(function() {
+		setTimeout(function() {
+			process.exit(0);
+		}, 15);
+	})
+	.catch(function() {
+		setTimeout(function() {
+			process.exit(1);
+		}, 15);
+	});
